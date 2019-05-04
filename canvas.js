58f325a78255e42dcc5e256892d578bd023d3b12
AS Paul <aspaul2@gmail.com>
2019-05-03 21:40:23 -0700

    Update canvas.js
    just cleaned things up a little to make it easier to read and use

--- canvas.js
+++ canvas.js
@@ -1,4 +1,3 @@
-
 var canvas= document.querySelector('canvas');
 //make canvas fill the entire window (though our grid does not necessarily have to)
 canvas.width= window.innerWidth;
@@ -28,8 +27,13 @@ for (var i=0; i<square_size*10; i+=square_size){
 
 draw_grid();
 
+//to feed into the draw line and draw circle function
+function get_point(cell){
+	return (square_size/2)+(square_size*cell);
+}
 
-//can be used to mark the position of the knight in each step once we have our path
+
+//these can be used to mark the position of the knight in each step once we have our path
 function draw_circle(x, y){
 	c.beginPath();
 	c.arc(x, y, 5, 0, Math.PI* 2, false);
@@ -37,22 +41,34 @@ function draw_circle(x, y){
 
 }
 
+//draw circles at a specific cell in the grid, for instance (0,0) or (2,4)
+function draw_circle_at_coordinate(x,y){
+	x_coord= get_point(x);
+	y_coord= get_point(y);
+	draw_circle(x_coord, y_coord);
+
+}
+
+
 //marks source point at 1,2 (indexed from zero)
 c.strokeStyle= 'blue';
-draw_circle((square_size/2)+square_size, (square_size/2)+ square_size*2);
+draw_circle_at_coordinate(1,2)
+
 
 //marks sink at 8,7 
 c.strokeStyle= 'red';
-draw_circle((square_size/2)+(square_size*8), (square_size/2)+ square_size*7);
+draw_circle_at_coordinate(8,7);
 
 
-//create 2d array of the flow capacity
+
+
+//create 2d array of the flow capacity (this function is already in the main file)
 function createGrid() {
   let gridRows = new Array(10);
   gridRows.fill(new Array(10).fill(0));
   gridRows = gridRows.map(row => {
     row = row.map(() => {
-      return Math.floor(Math.random()*15)*2; // random even number
+      return Math.floor(Math.random()*16)*2; // random even number
     })
     return row;
   });
@@ -62,13 +78,13 @@ function createGrid() {
 
 var flow_values= createGrid();
 
-//draws randomized source values on the canvas, saved in the 2d array above
+//draws randomized flow values on the canvas, saved in the 2d array above (make sure it is the same values that the algorithm is using)
 function draw_flow_capacity(){
-	for (var i=0; i<10; i++){//(var i= square_size/2; i<square_size*10; i+=square_size){
-		x_coordinate= i * square_size+20;
+	for (var i=0; i<10; i++){
+		x_coordinate= i * square_size+ square_size/3;
 		for (var j=0; j<10; j++){
 		 	c.font= "10px Arial";
-		 	c.fillText(flow_values[i][j], x_coordinate, j*square_size+12);
+		 	c.fillText(flow_values[i][j], x_coordinate, j*square_size+ square_size/5);
 		 	console.log("This is x" + x_coordinate);
 		 	
 		 }
@@ -79,19 +95,26 @@ function draw_flow_capacity(){
 
 draw_flow_capacity();
 
+
+
+
 //this is going to help draw our path, connecting the circles
 function draw_line(x_start, y_start, x_end, y_end){
 	c.beginPath();
-	c.moveTo(x_start, y_start);
-	c.lineTo(x_end, y_end);
+	c.moveTo(get_point(x_start), get_point(y_start));
+	c.lineTo(get_point(x_end), get_point(y_end));
 	c.strokeStyle= "#fa34a3";
 	c.stroke();
 }
 
 
 
+//just testing the draw line function
+draw_line(0,0, 2,4);
 
-//creates an animation loop, may use this to show position of knight moving
+
+
+//creates an animation loop, may use this to show position of knight as it moves along its path
 // function animate(){
 // 	requestAnimationFrame(animate);
 // 	// c.beginPath();
@@ -106,7 +129,6 @@ function draw_line(x_start, y_start, x_end, y_end){
 // 	x+=1;
 // }
 // animate();
-
 
 
 
